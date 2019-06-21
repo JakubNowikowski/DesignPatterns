c040c3df347f89c727068eb6a5f0c78b1128c79e
Jakub Nowikowski <kubanowikowski@gmail.com>
2019-06-21 11:34:36 +0200

    Prototype pattern

---
+++ PrototypePattern/Line.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+	public class Point
+	{
+		public int X, Y;
+
+		public Point(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+
+	public class Line
+	{
+		public Point Start, End;
+
+		public Line DeepCopy()
+		{
+			return new Line()
+			{
+				Start = new Point(Start.X, Start.Y),
+				End = new Point(End.X, End.Y)
+			};
+		}
+
+		public override string ToString()
+		{
+			return $"StartPoint: [{Start.X},{Start.Y}] EndPoint: [{End.X},{End.Y}]";
+		}
+	}
+}
