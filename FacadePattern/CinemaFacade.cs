019fe4595c153fc88b2d773cb0a43b7bf37e769b
Nowikowski <IOJNOWIK@zeiss.com>
2019-06-25 10:48:31 +0200

    Facade pattern added

---
+++ FacadePattern/CinemaFacade.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class CinemaFacade
+    {
+        Lights lights = new Lights();
+        Speakers speakers = new Speakers();
+        Projector projector = new Projector();
+
+        public void WatchMovie(string name)
+        {
+            Console.WriteLine("Preaparing all devices...\n");
+
+            lights.TurnOn();
+            lights.SetBrightness(10);
+
+            Console.WriteLine();
+
+            speakers.TurnOn();
+            speakers.SetVolume(40);
+
+            Console.WriteLine();
+
+            projector.TurnOn();
+
+            Console.WriteLine($"\n*** Start playing: {name} ***");
+        }
+
+        public void EndMovie()
+        {
+            Console.WriteLine("\nTurning off all devices...\n");
+
+            lights.TurnOff();
+
+            Console.WriteLine();
+
+            speakers.TurnOff();
+
+            Console.WriteLine();
+
+            projector.TurnOff();
+
+            Console.WriteLine($"\nSystem turned off");
+        }
+
+    }
+}
---
+++ FacadePattern/Lights.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class Lights
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Turning on lights");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Turning off lights");
+        }
+
+        public void SetBrightness(int val)
+        {
+            Console.WriteLine($"Setting brightness to {val}");
+        }
+    }
+}
