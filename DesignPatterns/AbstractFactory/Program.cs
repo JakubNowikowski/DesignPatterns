44feaa8eb8f659e1693d3e0a02974da611ff58d1
kuba <kubanowikowski@gmail.com>
2021-03-30 20:45:10 +0200

    Abstract facotry added

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+        }
+    }
+}
