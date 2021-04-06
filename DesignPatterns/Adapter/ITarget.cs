69538a0144c404305753419ba6ed2f35a849b285
kuba <kubanowikowski@gmail.com>
2021-04-06 20:59:15 +0200

    Adapter added

---
+++ DesignPatterns/Adapter/ITarget.cs
@@ -0,0 +1,8 @@
+﻿namespace RefactoringGuru.DesignPatterns.Adapter.Conceptual
+{
+    // The Target defines the domain-specific interface used by the client code.
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+}
--- DesignPatterns/Singleton/Singleton.cs
+++ DesignPatterns/Singleton/Singleton.cs
@@ -1,38 +1,67 @@
-﻿namespace Singleton
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingletonVsStaticClass
 {
-    // The Singleton class defines the `GetInstance` method that serves as an
-    // alternative to constructor and lets clients access the same instance of
-    // this class over and over.
-    class Singleton
+    class SomeClass
     {
-        // The Singleton's constructor should always be private to prevent
-        // direct construction calls with the `new` operator.
-        private Singleton() { }
+        public int ABC;
+    }
 
-        // The Singleton's instance is stored in a static field. There there are
-        // multiple ways to initialize this field, all of them have various pros
-        // and cons. In this example we'll show the simplest of these ways,
-        // which, however, doesn't work really well in multithreaded program.
-        private static Singleton _instance;
+    public sealed class SingletonSampleClass
+    {
+        static SingletonSampleClass _instance;
 
-        // This is the static method that controls the access to the singleton
-        // instance. On the first run, it creates a singleton object and places
-        // it into the static field. On subsequent runs, it returns the client
-        // existing object stored in the static field.
-        public static Singleton GetInstance()
+        public static SingletonSampleClass Instance
         {
-            if (_instance == null)
-            {
-                _instance = new Singleton();
-            }
-            return _instance;
+            get { return _instance ?? (_instance = new SingletonSampleClass()); }
         }
+        private SingletonSampleClass() { }
 
-        // Finally, any singleton should define some business logic, which can
-        // be executed on its instance.
-        public static void someBusinessLogic()
+        public string Message { get; set; }
+    }
+
+    static public class StaticSampleClass
+    {
+        private static readonly int SomeVariable;
+        //Static constructor is executed only once when the type is first used.   
+        //All classes can have static constructors, not just static classes.  
+        static StaticSampleClass()
         {
-            // ...
+            SomeVariable = 1;
+            //Do the required things  
+        }
+        public static string ShowValue()
+        {
+            return string.Format("The value of someVariable is {0}", SomeVariable);
+        }
+        public static string Message { get; set; }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Normal class instantiation and usage  
+            var someClass = new SomeClass { ABC = 5 };
+            Console.WriteLine("Normal class usage: " + someClass.ABC);
+
+            //Static Class instantiation  
+            string returnValue = StaticSampleClass.ShowValue();
+            Console.WriteLine("Static class usage: " + returnValue);
+
+            //Singleton class instantiation  
+            var singletonSampleClass = SingletonSampleClass.Instance;
+            singletonSampleClass.Message = "Hello";
+            Console.WriteLine("Singleton class usage: " + singletonSampleClass.Message);
+
+            //Test the instances if are equal  
+            var anotherSingletonSampleClass = SingletonSampleClass.Instance;
+            Console.WriteLine("Checking if both instances are same: " + singletonSampleClass.Equals(anotherSingletonSampleClass));
+
+            Console.Read();
         }
     }
 }
