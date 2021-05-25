f7bbd6f9081362d647df41893ed47347c7c8ae86
kuba <kubanowikowski@gmail.com>
2021-05-25 12:48:19 +0200

    test gitignore

--- ToBeIgnored/ConcreteCreator1.cs
+++ ToBeIgnored/ConcreteCreator1.cs
@@ -7,7 +7,7 @@
         // Note that the signature of the method still uses the abstract product
         // type, even though the concrete product is actually returned from the
         // method. This way the Creator can stay independent of concrete product
-        // classes.
+        // classes.fadsf
         public override IProduct FactoryMethod()
         {
             return new ConcreteProduct1();
